524afabbd18927aa5eb930af794f1b79eab3e4a9
Vlad B. <pocyk228@gmail.com>
2026-05-07 12:55:37 +0500

    May 7th update, preparing for test prod

--- backend/DjangoProj/DjangoProj/prod_settings.py
+++ backend/DjangoProj/DjangoProj/prod_settings.py
@@ -3,7 +3,7 @@ from .settings import *
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'pocyk228$yourdbname', 
+        'NAME': 'pocyk228$projdb', 
         'USER': 'pocyk228',
         'PASSWORD': 'denis19082002',
         'HOST': 'pocyk228.mysql.pythonanywhere-services.com',
