d698d447ad1f123b835b56a4d971334c58f6fdae
Vlad B. <pocyk228@gmail.com>
2026-05-08 12:41:10 +0500

    May 8th 1st update, media loading error fix

--- backend/DjangoProj/DjangoProj/urls.py
+++ backend/DjangoProj/DjangoProj/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('media/<path:path>', serve, {'document_root': settings.MEDIA_ROOT}),
+]
